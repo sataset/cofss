c79f07b6669ebe73803c2c212f908b79d6d08252
ARX <arx@iMac.local>
2017-11-04 21:54:16 +0700

    Fix equation

--- src/modules/hwp_qwp.cpp
+++ src/modules/hwp_qwp.cpp
@@ -2,13 +2,13 @@
 #include "../utility.h"
 
 HWP_QWP::HWP_QWP() {
-    name = "quarter_wp";
+    name = "hwp+qwp";
     psi = 0.056;
     xi = 0.1;
 }
 
 HWP_QWP::HWP_QWP(const double& in_psi, const double& in_xi) {
-    name = "quarter_wp";
+    name = "hwp+qwp";
     psi = in_psi;
     xi = in_xi;
 }
@@ -18,7 +18,7 @@ void HWP_QWP::execute(Field* signal) {}
 void HWP_QWP::execute(Polarizations* signal) {
     Field E_1 = signal->x();
     signal->right = E_1 * i_exp(psi) * std::cos(xi - math_pi / 4.0);
-    signal->left = E_1 * i_exp(psi) * std::cos(xi + math_pi / 4.0);
+    signal->left = E_1 * i_exp(-psi) * std::cos(xi + math_pi / 4.0);
 
     //Executor::instance()->enqueue(next, signal);
 }
