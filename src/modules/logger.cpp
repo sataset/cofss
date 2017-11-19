49868125417f0695e7ca67f2bba38f82e875e6e0
sataset <darmenoriginal@gmail.com>
2017-11-19 21:51:24 +0700

    Code refactor

--- src/modules/logger.cpp
+++ src/modules/logger.cpp
@@ -2,77 +2,99 @@
 
 Logger::Logger() { name = "logger"; }
 
-// void Logger::getLogs(std::ostream& os, bool for_field) {
-//     unsigned long size = 0;
-//     if (for_field) {
-//         size = field_storage.back().size();
-//         while (!field_storage.empty()) {
-//             for (unsigned long i = 0; i < size; ++i)
-//                 os << field_storage.back().w(i) << '\t'
-//                    << norm(field_storage.back()[i]) << std::endl;
-//             os << '\n' << std::endl;
-//         }
-//     } else {
-//         size = storage.back().right.size();
-//         while (!storage.empty()) {
-//             for (unsigned long i = 0; i < size; ++i)
-//                 os << storage.back().right.w(i) << '\t'
-//                    << norm(storage.back().right[i])
-//                    << storage.back().left.w(i) << '\t'
-//                    << norm(storage.back().left[i]) <<
-//                    std::endl;
-//             os << '\n' << std::endl;
-//         }
-//     }
-// }
-
-void Logger::getLogs(std::ostream& os) {
-    unsigned long size;
-    size = storage.back().right.size();
+void Logger::write_logs_to(std::ostream& os, WriteType wt) {
+    unsigned long size = storage.back().right.size();
     os.precision(15);
-    for (unsigned long i = 0; i < storage.size(); ++i) {
+
+    if (wt == TIME) {
+        RealVector time_domain(storage.front().right.time_grid());
+        for (unsigned long i = 0; i < storage.size(); ++i) {
+            for (unsigned long j = 0; j < size; ++j)
+                os << time_domain[j] << ','
+                << storage[i].right[j].real() << ','
+                << storage[i].right[j].imag() << ','
+                << storage[i].left[j].real() << ','
+                << storage[i].left[j].imag() << std::endl;
+            os << '\n' << std::endl;
+        }
+        return;
+    }
+
+    if (wt == FREQUENCY) {
+        RealVector frequency_domain(storage.front().right.frequency_grid());
+        RealVector power_right, power_left;
+
+        for (unsigned long i = 0; i < storage.size(); ++i) {
+        power_right = fft_shift(storage[i].right.spectral_power());
+        power_left = fft_shift(storage[i].left.spectral_power());
         for (unsigned long j = 0; j < size; ++j)
-            os << storage[i].right.t(j) << ','
-            << storage[i].right[j].real() << ','
-            << storage[i].right[j].imag() << ','
-            << storage[i].left[j].real() << ','
-            << storage[i].left[j].imag() << std::endl;
+            os << frequency_domain[j] << ','
+            << power_right[j] << ','
+            << power_left[j] << std::endl;
         os << '\n' << std::endl;
+        }
+        return;
     }
 }
 
-void Logger::getFirstNLast(std::ostream& os) {
-    unsigned long size;
-    size = storage.back().right.size();
+void Logger::write_first_to(std::ostream& os, WriteType wt) {
+    unsigned long size = storage.back().right.size();
     os.precision(15);
-    for (unsigned long i = 0; i < size; ++i)
-        os << storage.front().right.t(i) << ','
-        << storage.front().right[i].real() << ','
-        << storage.front().right[i].imag() << ','
-        << storage.front().left[i].real() << ','
-        << storage.front().left[i].imag() << std::endl;
-    os << '\n' << std::endl;
 
-    for (unsigned long i = 0; i < size; ++i)
-        os << storage.back().right.t(i) << ','
-        << storage.back().right[i].real() << ','
-        << storage.back().right[i].imag() << ','
-        << storage.back().left[i].real() << ','
-        << storage.back().left[i].imag() << std::endl;
-    os << '\n' << std::endl;
+    if (wt == TIME) {
+        RealVector time_domain(storage.front().right.time_grid());
+        for (unsigned long i = 0; i < size; ++i)
+            os << time_domain[i] << ','
+            << storage.front().right[i].real() << ','
+            << storage.front().right[i].imag() << ','
+            << storage.front().left[i].real() << ','
+            << storage.front().left[i].imag() << std::endl;
+        os << '\n' << std::endl;
+        return;
+    }
+
+    if (wt == FREQUENCY) {
+        RealVector frequency_domain(storage.front().right.frequency_grid());
+        RealVector power_right, power_left;
+        power_right = storage.front().right.spectral_power();
+        power_left = storage.front().left.spectral_power();
+        for (unsigned long j = 0; j < size; ++j)
+            os << frequency_domain[j] << ','
+            << power_right[j] << ','
+            << power_left[j] << std::endl;
+        os << '\n' << std::endl;
+        return;
+    }
 }
 
-void Logger::getCurrentState(std::ostream& os) {
-    unsigned long size;
-    size = storage.back().right.size();
+void Logger::write_last_to(std::ostream& os, WriteType wt) {
+    unsigned long size = storage.back().right.size();
     os.precision(15);
-      for (unsigned long i = 0; i < size; ++i)
-        os << storage.back().right.t(i) << ','
-        << storage.back().right[i].real() << ','
-        << storage.back().right[i].imag() << ','
-        << storage.back().left[i].real() << ','
-        << storage.back().left[i].imag() << std::endl;
-    os << '\n' << std::endl;
+
+    if (wt == TIME) {
+        RealVector time_domain(storage.front().right.time_grid());
+        for (unsigned long i = 0; i < size; ++i)
+            os << time_domain[i] << ','
+            << storage.back().right[i].real() << ','
+            << storage.back().right[i].imag() << ','
+            << storage.back().left[i].real() << ','
+            << storage.back().left[i].imag() << std::endl;
+        os << '\n' << std::endl;
+        return;
+    }
+
+    if (wt == FREQUENCY) {
+        RealVector frequency_domain(storage.front().right.frequency_grid());
+        RealVector power_right, power_left;
+        power_right = storage.back().right.spectral_power();
+        power_left = storage.back().left.spectral_power();
+        for (unsigned long j = 0; j < size; ++j)
+            os << frequency_domain[j] << ','
+            << power_right[j] << ','
+            << power_left[j] << std::endl;
+        os << '\n' << std::endl;
+        return;
+    }
 }
 
 void Logger::execute(Field* signal) {
--- src/modules/logger.h
+++ src/modules/logger.h
@@ -2,18 +2,21 @@
 #define LOGGER_H_
 
 #include "../core/module.h"
+#include "../utility.h"
 
 // Gathering logs from aimed modules
 
 class Logger : public Module {
     std::vector<Polarizations> storage;
+    bool is_in_time_domain;
   public:
     Logger();
-
+    enum WriteType { FREQUENCY, TIME };
     //void getLogs(std::ostream& os, bool for_field);
-    void getLogs(std::ostream& os);
-    void getFirstNLast(std::ostream& os);
-    void getCurrentState(std::ostream& os);
+
+    void write_logs_to(std::ostream& os, WriteType wt = TIME);
+    void write_first_to(std::ostream& os, WriteType wt = TIME);
+    void write_last_to(std::ostream& os, WriteType wt = TIME);
 
     void execute(Field* signal);
     void execute(Polarizations* signal);
