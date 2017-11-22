af8e3ac57be1d30bc5b6b93158d3ca661c614db6
sataset <darmenoriginal@gmail.com>
2017-11-22 22:53:56 +0700

    Fix gain

--- src/modules/dwnt.cpp
+++ src/modules/dwnt.cpp
@@ -1,38 +1,48 @@
 #include "dwnt.h"
 
-DWNT::DWNT() {
+DWNT::DWNT()
+{
     name = "dwnt-sa";
-    alpha_0 = 0.64;   // [%]
-    alpha_ns = 0.36;  // [%]
-    P_sat = 10;   // [W]
+    alpha_0 = 0.64; // [%]
+    alpha_ns = 0.36; // [%]
+    P_sat = 10; // [W]
 }
 
 DWNT::DWNT(const double& modulation_depth,
-           const double& saturation_peak_power,
-           const double& non_saturable_losses) {
+    const double& saturation_peak_power,
+    const double& non_saturable_losses)
+{
     name = "dwnt-sa";
     alpha_0 = modulation_depth;
     alpha_ns = non_saturable_losses;
     P_sat = saturation_peak_power;
 }
 
-void DWNT::execute(Field* signal) {
-    double P;  // |E_+|^2 + |E_-|^2
+void DWNT::execute(Field* signal)
+{
+    double P; // |E_+|^2 + |E_-|^2
     for (unsigned long i = 0; i < signal->size(); ++i) {
         P = 1.0 - alpha_ns - alpha_0 / (1.0 + norm(signal->at(i)) / P_sat);
-        if (P < 0) P = 0.0;
-        signal->at(i) *= P;
+        if (P < 0)
+            P = 0.0;
+        signal->at(i) *= sqrt(P);
     }
 }
 
-void DWNT::execute(Polarizations* signal) {
-    double P;  // |E_+|^2 + |E_-|^2
-    for (unsigned long i = 0; i < signal->right.size(); ++i) {
-        P = 1.0 - alpha_ns - alpha_0 /
-        (1.0 + (norm(signal->right[i]) + norm(signal->left[i])) / P_sat);
-        if (P < 0) P = 0.0;
-        signal->right[i] *= P;
-        signal->left[i] *= P;
+void DWNT::execute(Polarizations* signal)
+{
+    double P; // |E_+|^2 + |E_-|^2
+    int size = signal->right.size();
+    for (int i = 0; i < size; ++i) {
+        P = 1.0 - alpha_ns - alpha_0 / (1.0 + norm(signal->right[i] / P_sat));
+        if (P < 0)
+            P = 0.0;
+        signal->right[i] *= sqrt(P);
+
+        P = 1.0 - alpha_ns - alpha_0 / (1.0 + norm(signal->left[i]) / P_sat);
+        if (P < 0)
+            P = 0.0;
+        signal->left[i] *= sqrt(P);
     }
 
     // Executor::instance()->enqueue(next, signal);
