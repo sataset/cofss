0b27eeab662f3a5eb5e264495e255cec3ca9d921
ARX <arx@iMac.local>
2017-11-28 06:03:47 +0700

    Code refactor

--- src/modules/dwnt.cpp
+++ src/modules/dwnt.cpp
@@ -1,49 +1,32 @@
 #include "dwnt.h"
 
-DWNT::DWNT()
-{
+DWNT::DWNT() {
     name = "dwnt-sa";
-    alpha_0 = 0.64; // [%]
-    alpha_ns = 0.36; // [%]
-    P_sat = 10; // [W]
+    alpha_0 = 0.64;   // [%]
+    alpha_ns = 0.36;  // [%]
+    P_sat = 10;       // [W]
 }
 
 DWNT::DWNT(const double& modulation_depth,
-    const double& saturation_peak_power,
-    const double& non_saturable_losses)
-{
+           const double& saturation_peak_power,
+           const double& non_saturable_losses) {
     name = "dwnt-sa";
     alpha_0 = modulation_depth;
     alpha_ns = non_saturable_losses;
     P_sat = saturation_peak_power;
 }
 
-void DWNT::execute(Field* signal)
-{
-    double P; // |E_+|^2 + |E_-|^2
-    for (unsigned long i = 0; i < signal->size(); ++i) {
-        P = 1.0 - alpha_ns - alpha_0 / (1.0 + norm(signal->at(i)) / P_sat);
-        if (P < 0)
-            P = 0.0;
-        signal->at(i) *= sqrt(P);
+void DWNT::execute(Field* signal) {
+    double absorption;  // |E_+|^2 + |E_-|^2
+    int size = signal->size();                                    
+    for (int i = 0; i < size; ++i) {
+        absorption = alpha_ns + alpha_0 / (1.0 + norm(signal->at(i)) / P_sat);
+        if (absorption > 1.0) absorption = 1.0;
+        signal->at(i) *= sqrt(1.0 - absorption);
     }
 }
 
-void DWNT::execute(Polarizations* signal)
-{
-    double P; // |E_+|^2 + |E_-|^2
-    int size = signal->right.size();
-    for (int i = 0; i < size; ++i) {
-        P = 1.0 - alpha_ns - alpha_0 / (1.0 + norm(signal->right[i] / P_sat));
-        if (P < 0)
-            P = 0.0;
-        signal->right[i] *= sqrt(P);
-
-        P = 1.0 - alpha_ns - alpha_0 / (1.0 + norm(signal->left[i]) / P_sat);
-        if (P < 0)
-            P = 0.0;
-        signal->left[i] *= sqrt(P);
-    }
-
-    // Executor::instance()->enqueue(next, signal);
+void DWNT::execute(Polarizations* signal) {
+    execute(&signal->right);
+    execute(&signal->left);
 }
