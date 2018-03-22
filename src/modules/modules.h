d1adb5c25b7f362abf6a77ffb0217975c370ce33
Mukhtar Urynbassarov <arx@Mukhtars-iMac.local>
2018-03-22 13:41:41 +0700

    Rename to modules.h

---
+++ src/modules/modules.h
@@ -0,0 +1,14 @@
+#ifndef MODULES_PACK_H_
+#define MODULES_PACK_H_
+
+#include "counter.h"
+#include "coupler.h"
+#include "dwnt.h"
+#include "fiber.h"
+#include "logger.h"
+#include "pd_iso.h"
+#include "hwp_qwp.h"
+#include "active_fiber.h"
+#include "amplifier_simple.h"
+
+#endif  // MODULES_PACK_H_
