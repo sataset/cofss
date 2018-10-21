fbfeddd25d1b7e848c25721a96938dd3ca9376b8
Mukhtar Urynbassarov <arx@Mukhtars-iMac.local>
2018-10-21 07:29:10 +0700

    Add utilitary functions

--- src/modules/logger.cpp
+++ src/modules/logger.cpp
@@ -2,19 +2,75 @@
 
 Logger::Logger() { name = "logger"; }
 
-void Logger::write_logs_to(std::ostream& os, WriteType wt) {
+void Logger::setDelimeter(char delimeter) {
+    delimeter_is_defined = true;
+    delimeter_ = delimeter;
+}
+
+unsigned long Logger::size() { return storage.size(); }
+
+void Logger::clean() { storage.clear(); }
+
+std::vector<Polarizations>& Logger::getStorage() { return storage; }
+
+Polarizations& Logger::get_first() { return storage.front(); }
+
+Polarizations& Logger::get_last() { return storage.back(); }
+
+void Logger::export_metadata(std::ostream& os) {
+    if (!delimeter_is_defined) {
+        delimeter_ = ',';
+        delimeter_is_defined = true;
+    }
+    os.precision(15);
+    os << "# \"delim\" time_steps time_step" << std::endl;
+    os << "# \"" << delimeter_ << "\" " << storage.back().right.size() << " "
+       << storage.back().right.getTimeStep() << std::endl;
+}
+
+void Logger::export_to_heatmap(std::ostream& os) {
+    if (!delimeter_is_defined) {
+        delimeter_ = ',';
+        delimeter_is_defined = true;
+    }
+    unsigned long size = storage.back().right.size();
+    os.precision(15);
+    RealVector time_domain(storage.front().right.time_grid());
+    for (unsigned long i = 0; i < storage.size(); ++i) {
+        for (unsigned long j = 0; j < size; ++j) {
+            os << std::norm(storage[i].right[j]);
+            if (j != size - 1) os << delimeter_;
+        }
+        os << '\n';
+    }
+    os << "\n\n";
+    for (unsigned long i = 0; i < storage.size(); ++i) {
+        for (unsigned long j = 0; j < size; ++j) {
+            os << std::norm(storage[i].left[j]);
+            if (j != size - 1) os << delimeter_;
+        }
+        os << '\n' << std::endl;
+    }
+}
+
+void Logger::export_every_to(int n, std::ostream& os, WriteType wt) {
+    if (!delimeter_is_defined) {
+        delimeter_ = ',';
+        delimeter_is_defined = true;
+    }
     unsigned long size = storage.back().right.size();
     os.precision(15);
 
     if (wt == TIME) {
         RealVector time_domain(storage.front().right.time_grid());
-        for (unsigned long i = 0; i < storage.size(); ++i) {
+        for (unsigned long i = 0; i < storage.size(); i += (n - 1)) {
             for (unsigned long j = 0; j < size; ++j)
-                os << time_domain[j] << ',' << storage[i].right[j].real() << ','
-                   << storage[i].right[j].imag() << ','
-                   << storage[i].left[j].real() << ','
+                os << time_domain[j] << delimeter_ << storage[i].right[j].real()
+                   << delimeter_ << storage[i].right[j].imag() << delimeter_
+                   << storage[i].left[j].real() << delimeter_
                    << storage[i].left[j].imag() << std::endl;
             os << '\n' << std::endl;
+            // os << '\n';
         }
         return;
     }
@@ -23,28 +79,33 @@ void Logger::write_logs_to(std::ostream& os, WriteType wt) {
         RealVector frequency_domain(storage.front().right.frequency_grid());
         RealVector power_right, power_left;
 
-        for (unsigned long i = 0; i < storage.size(); ++i) {
+        for (unsigned long i = 0; i < storage.size(); i += n) {
             power_right = fft_shift(storage[i].right.spectral_power());
             power_left = fft_shift(storage[i].left.spectral_power());
             for (unsigned long j = 0; j < size; ++j)
-                os << frequency_domain[j] << ',' << power_right[j] << ','
-                   << power_left[j] << std::endl;
+                os << frequency_domain[j] << delimeter_ << power_right[j]
+                   << delimeter_ << power_left[j] << std::endl;
             os << '\n' << std::endl;
         }
         return;
     }
 }
 
-void Logger::write_first_to(std::ostream& os, WriteType wt) {
+void Logger::export_first_to(std::ostream& os, WriteType wt) {
+    if (!delimeter_is_defined) {
+        delimeter_ = ',';
+        delimeter_is_defined = true;
+    }
     unsigned long size = storage.back().right.size();
     os.precision(15);
 
     if (wt == TIME) {
         RealVector time_domain(storage.front().right.time_grid());
         for (unsigned long i = 0; i < size; ++i)
-            os << time_domain[i] << ',' << storage.front().right[i].real()
-               << ',' << storage.front().right[i].imag() << ','
-               << storage.front().left[i].real() << ','
+            os << time_domain[i] << delimeter_
+               << storage.front().right[i].real() << delimeter_
+               << storage.front().right[i].imag() << delimeter_
+               << storage.front().left[i].real() << delimeter_
                << storage.front().left[i].imag() << std::endl;
         os << '\n' << std::endl;
         return;
@@ -56,23 +117,27 @@ void Logger::write_first_to(std::ostream& os, WriteType wt) {
         power_right = fft_shift(storage.front().right.spectral_power());
         power_left = fft_shift(storage.front().left.spectral_power());
         for (unsigned long j = 0; j < size; ++j)
-            os << frequency_domain[j] << ',' << power_right[j] << ','
-               << power_left[j] << std::endl;
+            os << frequency_domain[j] << delimeter_ << power_right[j]
+               << delimeter_ << power_left[j] << std::endl;
         os << '\n' << std::endl;
         return;
     }
 }
 
-void Logger::write_last_to(std::ostream& os, WriteType wt) {
+void Logger::export_last_to(std::ostream& os, WriteType wt) {
+    if (!delimeter_is_defined) {
+        delimeter_ = ',';
+        delimeter_is_defined = true;
+    }
     unsigned long size = storage.back().right.size();
     os.precision(15);
 
     if (wt == TIME) {
         RealVector time_domain(storage.front().right.time_grid());
         for (unsigned long i = 0; i < size; ++i)
-            os << time_domain[i] << ',' << storage.back().right[i].real() << ','
-               << storage.back().right[i].imag() << ','
-               << storage.back().left[i].real() << ','
+            os << time_domain[i] << delimeter_ << storage.back().right[i].real()
+               << delimeter_ << storage.back().right[i].imag() << delimeter_
+               << storage.back().left[i].real() << delimeter_
                << storage.back().left[i].imag() << std::endl;
         os << '\n' << std::endl;
         return;
@@ -84,16 +149,54 @@ void Logger::write_last_to(std::ostream& os, WriteType wt) {
         power_right = fft_shift(storage.back().right.spectral_power());
         power_left = fft_shift(storage.back().left.spectral_power());
         for (unsigned long j = 0; j < size; ++j)
-            os << frequency_domain[j] << ',' << power_right[j] << ','
-               << power_left[j] << std::endl;
+            os << frequency_domain[j] << delimeter_ << power_right[j]
+               << delimeter_ << power_left[j] << std::endl;
         os << '\n' << std::endl;
         return;
     }
 }
 
-Polarizations& Logger::get_last() { return storage.back(); }
+void Logger::import_from(std::istream& is) {
+    clean();
+    unsigned long field_size;
+    double time_step;
+    char skip;
+    // # "delim" time_steps time_step storage_size
+    // # "," 8192 0.5 2
+    is.ignore(std::numeric_limits<std::streamsize>::max(), is.widen('\n'));
+    is >> skip >> skip >> delimeter_ >> skip >> field_size >> time_step;
+    delimeter_is_defined = true;
 
-void Logger::write_derivative_to(std::ostream& os) {
+    Field init(field_size);
+    init.setTimeStep(time_step);
+
+    double re, im;
+    unsigned long i = 1;
+    unsigned long file_end = std::count(std::istreambuf_iterator<char>(is),
+                                        std::istreambuf_iterator<char>(),
+                                        '\n');
+    is.clear();
+    is.seekg(0, std::ios::beg);
+    is.ignore(std::numeric_limits<std::streamsize>::max(), is.widen('\n'));
+    is.ignore(std::numeric_limits<std::streamsize>::max(), is.widen('\n'));
+    do {
+        storage.push_back({init, init});
+        for (unsigned long j = 0; j < field_size; ++j) {
+            is.ignore(std::numeric_limits<std::streamsize>::max(),
+                      is.widen(delimeter_));
+            is >> re >> delimeter_ >> im >> delimeter_;
+            storage.back().right[j] = Complex(re, im);
+            is >> re >> delimeter_ >> im;
+            storage.back().left[j] = Complex(re, im);
+        }
+        is.ignore(std::numeric_limits<std::streamsize>::max(), is.widen('\n'));
+        is.ignore(std::numeric_limits<std::streamsize>::max(), is.widen('\n'));
+        i += field_size + 2;
+    } while (i < file_end);
+}
+
+void Logger::export_derivative_to(std::ostream& os) {
+    if (!delimeter_is_defined) delimeter_ = ',';
     unsigned long size = storage.back().right.size();
     double time_step = storage.back().right.getTimeStep();
 
@@ -114,11 +217,11 @@ void Logger::write_derivative_to(std::ostream& os) {
         dsignal_l[i] = (p_l[i + 1] - p_l[i - 1]) / 2.0 / time_step;
 
     for (unsigned long i = 0; i < size; ++i)
-        os << dsignal_r[i] << ',' << dsignal_l[i] << std::endl;
+        os << dsignal_r[i] << delimeter_ << dsignal_l[i] << std::endl;
 }
 
 void Logger::execute(Field* signal) {
-    storage.push_back(Polarizations{*signal, Field(signal->size(), 0)});
+    storage.push_back({*signal, Field(signal->size(), 0)});
 }
 
 void Logger::execute(Polarizations* signal) { storage.push_back(*signal); }
--- src/modules/logger.h
+++ src/modules/logger.h
@@ -8,16 +8,26 @@
 
 class Logger : public Module {
     std::vector<Polarizations> storage;
+    char delimeter_;
+    bool delimeter_is_defined;
   public:
     Logger();
     enum WriteType { FREQUENCY, TIME };
-
-    void write_logs_to(std::ostream& os, WriteType wt = TIME);
-    void write_first_to(std::ostream& os, WriteType wt = TIME);
-    void write_last_to(std::ostream& os, WriteType wt = TIME);
-
+    void setDelimeter(char delimeter);
+    unsigned long size();
+    void clean();
+    std::vector<Polarizations>& getStorage();
+    Polarizations& get_first();
     Polarizations& get_last();
-    void write_derivative_to(std::ostream& os);
+
+    void export_metadata(std::ostream& os);
+    void export_to_heatmap(std::ostream& os);
+    void export_every_to(int n, std::ostream& os, WriteType wt = TIME);
+    void export_first_to(std::ostream& os, WriteType wt = TIME);
+    void export_last_to(std::ostream& os, WriteType wt = TIME);
+    void import_from(std::istream& is);
+
+    void export_derivative_to(std::ostream& os);
 
     void execute(Field* signal);
     void execute(Polarizations* signal);
